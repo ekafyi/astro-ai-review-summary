f0fc14957af85e8d140bb293541e6563a07c0d0d
Eka <6597211+ekafyi@users.noreply.github.com>
2025-06-30 18:36:34 +0700

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
-
-  integrations: [react()],
-  adapter: vercel(),
+	integrations: [react()],
+	adapter: vercel(),
 });
