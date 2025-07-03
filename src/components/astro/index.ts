4d43393843e905172bb448b027e5d26e1088bf51
Eka <6597211+ekafyi@users.noreply.github.com>
2025-07-03 22:52:48 +0700

    refactor components for nesting server islands (fetch reviews then generate ai summary)

--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -1,4 +1,6 @@
+export { default as AISummaryIsland } from "./ai-summary-island.astro";
 export { default as ProductImage } from "./product-image.astro";
 export { default as ReviewsSectionIsland } from "./reviews-section-island.astro";
+export { default as ReviewsWrapper } from "./reviews-wrapper.astro";
 export { default as SiteFooter } from "./site-footer.astro";
 export { default as SiteHeader } from "./site-header.astro";
