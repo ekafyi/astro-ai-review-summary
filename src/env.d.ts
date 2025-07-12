1924c67395ff6fdf25d22ae3b03fd631a72e7255
Eka <6597211+ekafyi@users.noreply.github.com>
2025-07-12 20:06:02 +0700

    feat: add optional analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
 	readonly GOOGLE_GENERATIVE_AI_API_KEY: string;
 	readonly UPSTASH_KV_REST_API_URL: string;
 	readonly UPSTASH_KV_REST_API_TOKEN: string;
+	readonly UMAMI_WEBSITE_ID?: string;
 }
 
 interface ImportMeta {
